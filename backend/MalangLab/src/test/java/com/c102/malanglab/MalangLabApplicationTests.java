39b4deef2520c66c4b2a7c7292e12678f6d0a9aa
Taemin Im <devTaemin@gmail.com>
2023-04-26 15:41:01 +0900

    begin : [BE] malanglab initial commit

---
+++ backend/MalangLab/src/test/java/com/c102/malanglab/MalangLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c102.malanglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalangLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
