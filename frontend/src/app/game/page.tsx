8dfc732acdfc8f504dc81bbd231d53d0576d9e5b
choizlor <choizlor@gmail.com>
2023-04-27 17:53:01 +0900

    feat : [FE] countdown 완성

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -1,3 +1,36 @@
+'use client';
+
+import Blur from '@/components/common/Blur';
+import CountDown from '@/components/game/CountDown';
+import WordNum from '@/components/game/WordNum';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
 export default function GamePage() {
-  return <div></div>;
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShow(false);
+    }, 3200);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div
+      className="min-h-screen bg-cover flex flex-col align-middle justify-center items-center"
+      style={{ backgroundImage: "url('/imgs/bg-1.png')" }}
+    >
+      {show ? (
+        <>
+          <Blur />
+          <CountDown />
+        </>
+      ) : (
+        <>
+          <WordNum num={1004}/>
+          <Image src={'/imgs/word.png'} alt="word" width={800} height={500} />
+        </>
+      )}
+    </div>
+  );
 }
