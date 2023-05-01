508f4ff0064fd91af56e832619cb7956bc32b9bd
choizlor <choizlor@gmail.com>
2023-05-01 14:31:25 +0900

    design : [FE] 게임페이지 디자인 수정

--- frontend/src/components/common/AlertBox.tsx
+++ frontend/src/components/common/AlertBox.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function AlertBox({ text }: Props) {
   return (
-    <div className="bg-white shadow-[7px_7px_10px_rgba(0,0,0,0.25)] rounded-[10px] absolute w-[50vw]">
+    <div className="bg-white shadow-[7px_7px_10px_rgba(0,0,0,0.25)] rounded-[10px] absolute w-[50vw] z-20">
       <Image src={'/imgs/tulip.png'} width={100} height={50} alt="tulip" className='absolute top-[-35px] left-[-20px]' />
       <Image src={'/imgs/tulip.png'} width={100} height={50} alt="tulip" className='absolute top-24 right-[-30px]' />
       <h1 className="font-bold text-[#44474B] text-[2rem] py-10 text-center sm:text-[3rem]">
