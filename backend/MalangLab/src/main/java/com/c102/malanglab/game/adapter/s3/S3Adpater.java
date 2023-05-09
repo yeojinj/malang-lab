2ff095f9a1df135be3660ca7f9fb3529f30f1710
Taemin Im <devTaemin@gmail.com>
2023-05-09 13:52:01 +0900

    update : [BE] S3 어댑터 컴포넌트 등록

--- backend/MalangLab/src/main/java/com/c102/malanglab/game/adapter/s3/S3Adpater.java
+++ backend/MalangLab/src/main/java/com/c102/malanglab/game/adapter/s3/S3Adpater.java
@@ -1,8 +1,12 @@
 package com.c102.malanglab.game.adapter.s3;
 
 import com.c102.malanglab.game.application.port.out.S3Port;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Component
+@RequiredArgsConstructor
 public class S3Adpater implements S3Port {
 
     private S3Uploader s3Uploader;
