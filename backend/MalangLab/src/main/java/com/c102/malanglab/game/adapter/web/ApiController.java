a45572102315260b3e079a2318c3959747c2b0e5
Taemin Im <devTaemin@gmail.com>
2023-05-09 17:45:36 +0900

    update : [BE] api 함수명 변경

--- backend/MalangLab/src/main/java/com/c102/malanglab/game/adapter/web/ApiController.java
+++ backend/MalangLab/src/main/java/com/c102/malanglab/game/adapter/web/ApiController.java
@@ -50,7 +50,7 @@ public class ApiController {
      * @return
      */
     @PostMapping("/{roomId}")
-    public ResponseEntity<GuestRequest> enter(@PathVariable Long roomId, @RequestBody GuestRequest guestRequest) {
+    public ResponseEntity<GuestRequest> register(@PathVariable Long roomId, @RequestBody GuestRequest guestRequest) {
         return new CustomResponseEntity(HttpStatus.OK, gameStatusCase.register(roomId, guestRequest)).convertToResponseEntity();
     }
 
