b91d0922db64081606c4702595ba096394d47e9a
choizlor <choizlor@gmail.com>
2023-05-09 09:18:54 +0900

    design : [FE] 404 페이지 수정

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image';
-import notfound from '../../public/imgs/notfound.png'
-
 export default function NotFound() {
+  const notfound = 'https://s3.ap-northeast-2.amazonaws.com/static.malang-lab.com/static/notfound.png'
   return <div
-    className="min-h-screen bg-cover bg-center flex flex-col align-middle justify-center"
-    style={{ backgroundImage: "url('/imgs/bg-4.png')" }}
+    className="min-h-screen bg-cover bg-center flex flex-col align-middle justify-center bg-bg-4"
   >
     <div className='flex flex-col'>
-      <Image src={notfound} className="mx-auto w-[60%]" alt='' />
+      <img src={notfound} className="mx-auto w-[50%]" alt='' />
       <a href="/" className='button-black w-[200px] text-center flex align-middle hover:scale-[1.02] hover:bg-white hover:text-black'><p className='m-auto'>홈으로</p></a>
     </div>
   </div>;
