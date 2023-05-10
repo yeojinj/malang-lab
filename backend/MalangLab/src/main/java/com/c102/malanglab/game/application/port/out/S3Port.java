15fe31bb151568f0efbe093013f0cb0eb55f0d82
yogurt05j <yogurt05j@gmail.com>
2023-05-10 14:05:09 +0900

    feat : [BE] S3 이미지 저장 경로 설정

--- backend/MalangLab/src/main/java/com/c102/malanglab/game/application/port/out/S3Port.java
+++ backend/MalangLab/src/main/java/com/c102/malanglab/game/application/port/out/S3Port.java
@@ -3,5 +3,5 @@ package com.c102.malanglab.game.application.port.out;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Port {
-    String setImgPath(MultipartFile image);
+    String setImgPath(MultipartFile image, String path);
 }
