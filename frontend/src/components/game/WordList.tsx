d7b3336fe4becb561ac7d63c23e3686231e98e53
choizlor <choizlor@gmail.com>
2023-05-11 14:18:26 +0900

    feat : 게임 word 입력 axios 연결

--- frontend/src/components/game/WordList.tsx
+++ frontend/src/components/game/WordList.tsx
@@ -3,12 +3,29 @@
 import Image from 'next/image';
 import WordsGrid from './WordsGrid';
 import { useRef, useState, useEffect } from 'react';
+import { useSocket } from '@/context/SocketContext';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
+import { inputWord } from '@/apis/apis';
+import { WordInfo } from '@/store/Types';
 
 export default function WordList() {
   const scrollRef = useRef<HTMLDivElement>(null);
   const playerRef = useRef<HTMLAudioElement>(null);
   const [word, setWord] = useState<string>('');
   const [words, setWords] = useState<string[]>([]);
+  const { publishUpdate } = useSocket();
+  // const roomId = useSelector((state: RootState) => state.guest.pin);
+  const roomId = '195048'
+
+  const wordinfo: WordInfo = {
+    word,
+    time: new Date().getTime()
+  }
+
+  const handlePostWord = async () => {
+    const res = await inputWord(wordinfo);
+  }
 
   useEffect(() => {
     // 가장 밑으로 스크롤
@@ -17,20 +34,30 @@ export default function WordList() {
     }
     // 효과음
     playerRef.current?.play();
+    console.log(roomId)
   }, [words]);
+
+  const destination = `/queue/manager/room.${roomId}`;
+  const type = 'CHECK_DB';
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       if (word.trim() === '') {
         alert('단어를 입력해주세요.');
-      } else if (words.includes(word)) {
-        alert('중복된 단어입니다!');
-        setWord('')
-      } else if (word.length > 13) {
-        alert('13자 이내로 입력해주세요.')
-        setWord(word.slice(0,13))
       }
-      else {
+      // else if (word == keyword) {
+      //   alert('제시어와 같습니다.')
+      //   setWord('');
+      // }
+      else if (words.includes(word)) {
+        alert('중복된 단어입니다!');
+        setWord('');
+      } else if (word.length > 13) {
+        alert('13자 이내로 입력해주세요.');
+        setWord(word.slice(0, 13));
+      } else {
+        // publishUpdate(destination, type); // 단어 추가될 때마다 전송
+        handlePostWord()
         setWords([...words, word]);
         setWord('');
       }
