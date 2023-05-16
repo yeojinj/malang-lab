6df04c931d4e78857a70634744f328fc182d6982
choizlor <choizlor@gmail.com>
2023-05-16 21:35:52 +0900

    feat : [FE] 수상 페이지 구성

--- frontend/src/components/award/HostAwardItem.tsx
+++ frontend/src/components/award/HostAwardItem.tsx
@@ -2,45 +2,22 @@ import Image from 'next/image';
 import Ribbon from './Ribbon';
 import Confetti from './Confetti';
 import { AwardInfo } from '@/store/Types';
+import { award } from '@/data/awarddata';
 
 type Props = {
   awardInfo: AwardInfo;
 };
 
 export default function HostAwardItem({ awardInfo }: Props) {
-  const award = {
-    IDEA_MACHINE: {
-      title: '단어를 가장 많이 입력한 말랑이',
-      imagePath:
-        'https://s3.ap-northeast-2.amazonaws.com/static.malang-lab.com/static/snowman.png',
-    },
-    HIDDEN_FASTER: {
-      title: '히든 단어를 가장 빨리 찾은 말랑이',
-      imagePath:
-        'https://s3.ap-northeast-2.amazonaws.com/static.malang-lab.com/static/cat-malang.png',
-    },
-    LAST_FIGHTER: {
-      title: '맨 마지막까지 최선을 다한 말랑이',
-      imagePath:
-        'https://s3.ap-northeast-2.amazonaws.com/static.malang-lab.com/static/pingu.png',
-    },
-    QUICK_THINKER: {
-      title: '빈도수 가장 높은 단어 먼저 쓴 말랑이',
-      imagePath:
-        'https://s3.ap-northeast-2.amazonaws.com/static.malang-lab.com/static/frog.png',
-    },
-  };
-
-  console.log(awardInfo, 'awardinfo')
-
+  
   return (
     <div>
       <Confetti />
-      <p className="text-black font-bold text-[2rem]">
-        {award[awardInfo?.type].title}
+      <p className="text-black font-bold text-[2rem] mt-8">
+        {award[awardInfo?.type]?.title}
       </p>
       <Image
-        src={award[awardInfo?.type].imagePath}
+        src={award[awardInfo?.type]?.imagePath}
         width={500}
         height={500}
         alt="award"
