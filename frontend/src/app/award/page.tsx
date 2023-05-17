43fa3973bd3737b97275d33802d6e6beabd6987b
choizlor <choizlor@gmail.com>
2023-05-17 09:18:38 +0900

    fix : [FE] useSelector error 해결

--- frontend/src/app/award/page.tsx
+++ frontend/src/app/award/page.tsx
@@ -15,19 +15,23 @@ export default function Page() {
   const isHost: boolean = useSelector(
     (state: RootState) => state.status.isHost,
   );
-  const pin = isHost
-    ? useSelector((state: RootState) => state.gameinfo.id)
-    : useSelector((state: RootState) => state.guest.pin);
+  const hostPin = useSelector((state: RootState) => state.gameinfo.id)
+  const guestPin = useSelector((state: RootState) => state.guest.pin);
   const [awardDatas, setAwardDatas] = useState<AwardInfo[]>([]);
 
   useEffect(() => {
     // award 데이터 받아오기
     const handleAward = async () => {
-      const res = await awardsApi(pin);
-      setAwardDatas(res);
+      if (isHost) {
+        const res = await awardsApi(hostPin);
+        setAwardDatas(res);
+      } else {
+        const res = await awardsApi(guestPin);
+        setAwardDatas(res);
+      }
     };
     handleAward();
-  }, [pin]);
+  }, [hostPin, guestPin]);
 
   return (
     <div className="w-[100vw] min-h-[100vh] bg-cover bg-center flex justify-center align-middle bg-bg-2 items-center">
